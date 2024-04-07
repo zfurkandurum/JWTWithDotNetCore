a0b4537c5e5aadfff7a47bae210ba10697ef96a2
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2024-04-07 23:20:10 +0300

    DTOs created

---
+++ JWTWithDotNetCore/DTOs/LoginDto.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace JWTWithDotNetCore.DTOs;
+
+public class LoginDto
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
