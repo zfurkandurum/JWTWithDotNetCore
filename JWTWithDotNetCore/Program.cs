9e1eb213fd17707487f52de616623d6b5634ca94
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2024-04-07 17:12:51 +0300

    Initial commit

---
+++ JWTWithDotNetCore/Program.cs
@@ -0,0 +1,41 @@
+using JWTWithDotNetCore.Core.Contextes;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Database"));
+});
+
+//Identity
+builder.Services
+    .AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+ 
+app.UseAuthentication();
+
+app.MapControllers();
+
+app.Run();
