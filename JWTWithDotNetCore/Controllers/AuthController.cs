a0ce23bff3bb47790f4b3d60fe73bcc83de07b4c
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2024-04-09 18:54:14 +0300

    Login Method Created

--- JWTWithDotNetCore/Controllers/AuthController.cs
+++ JWTWithDotNetCore/Controllers/AuthController.cs
@@ -1,7 +1,11 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using JWTWithDotNetCore.DTOs;
 using JWTWithDotNetCore.Roles;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace JWTWithDotNetCore.Controllers;
 
@@ -63,5 +67,58 @@ public class AuthController : ControllerBase
         await _userManager.AddToRoleAsync(newUser, StaticUserRoles.USER);
 
         return Ok("user created");
+    }
+
+    [HttpPost]
+    [Route("login")]
+    public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+    {
+        var user = await _userManager.FindByNameAsync(loginDto.UserNameOrEmail);
+        
+        if (user == null)
+            user = await _userManager.FindByEmailAsync(loginDto.UserNameOrEmail);
+        
+        if (user == null)
+            return BadRequest("user not found");
+
+        var isPasswordCorrect = await _userManager.CheckPasswordAsync(user , loginDto.Password);
+        
+        if(!isPasswordCorrect)
+            return BadRequest("password isn't correct");
+
+        var userRoles = await _userManager.GetRolesAsync(user);
+
+        var authClaims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, user.UserName),
+            new Claim(ClaimTypes.NameIdentifier, user.Id),
+            new Claim("JWTID", Guid.NewGuid().ToString())
+        };
+
+        foreach (var userRole in userRoles)
+        {
+            authClaims.Add(new Claim(ClaimTypes.Role,userRole));
+        }
+
+        var token = GenerateNewJsonWebToken(authClaims);
+        
+        return Ok(token); 
+    }
+    
+    private string GenerateNewJsonWebToken(List<Claim> claims)
+    {
+        var authSecret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Key"]));
+
+        var tokenObject = new JwtSecurityToken(
+            issuer: _configuration["JWT:ValidIssuer"],
+            audience: _configuration["JWT:ValidAudience"],
+            expires: DateTime.Now.AddHours(1),
+            claims: claims,
+            signingCredentials: new SigningCredentials(authSecret, SecurityAlgorithms.HmacSha256)
+            );
+
+        var token = new JwtSecurityTokenHandler().WriteToken(tokenObject);
+
+        return token;
     }
 }
